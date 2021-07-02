1bc4a2a3cef2ef351e7d0f3bb1d4930167a9ca8b
Max <kmaks1364@gmail.com>
2021-07-02 18:43:00 +0300

    Add project files.

---
+++ ToDoAppCore/Controllers/TasksController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ToDoAppCore.Data;
+using ToDoAppCore.Helpers;
+using ToDoAppCore.Models;
+using static ToDoAppCore.Helpers.Helper;
+
+namespace ToDoAppCore.Controllers
+{
+    public class TasksController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TasksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tasks
+        public async Task<IActionResult> Index()
+        {
+            var tasks = await _context.Tasks.ToListAsync();
+
+            foreach (var task in tasks)
+            {
+                if (task.Status != true)
+                {
+                    var res = DateTime.Compare(task.DateEnd, DateTime.Now);
+                    if (res <= 0)
+                        task.Status = null;
+                    else
+                        task.Status = false;
+                    _context.Tasks.Update(task);
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            return View(tasks);
+        }
+
+        // GET: Tasks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var taskModel = await _context.Tasks
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (taskModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(taskModel);
+        }
+
+        // GET: Tasks/Create
+        [NoDirectAccess]
+        public async Task<IActionResult> AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new TaskModel());
+
+            var taskModel = await _context.Tasks.FindAsync(id);
+            if (taskModel == null)
+            {
+                return NotFound();
+            }
+            return View(taskModel);
+        }
+
+        // POST: Tasks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit(int id, [Bind("Id,Title,Description,DateIssue,DateEnd,Status")] TaskModel taskModel)
+        {
+            if (ModelState.IsValid)
+            {
+                //Insert
+                if (id == 0)
+                {
+                    taskModel.Status = false;
+                    _context.Add(taskModel);
+                    await _context.SaveChangesAsync();
+                }
+                //Update
+                else
+                {
+                    try
+                    {
+                        _context.Update(taskModel);
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!TaskModelExists(taskModel.Id))
+                        { return NotFound(); }
+                        else
+                        { throw; }
+                    }
+                }
+                return Json(new { isValid = true, html = Helper.RenderRazorViewToString(this, "_ViewAll", _context.Tasks.ToList()) });
+            }
+            return Json(new { isValid = false, html = Helper.RenderRazorViewToString(this, "AddOrEdit", taskModel) });
+        }
+
+        // GET: Tasks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var taskModel = await _context.Tasks
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (taskModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(taskModel);
+        }
+
+        // POST: Tasks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var taskModel = await _context.Tasks.FindAsync(id);
+            _context.Tasks.Remove(taskModel);
+            await _context.SaveChangesAsync();
+            return Json(new { html = Helper.RenderRazorViewToString(this, "_ViewAll", _context.Tasks.ToList()) });
+        }
+
+        [HttpGet]
+        [Route("{Controller}/{Action}/{id}/{status}")]
+        public IActionResult ChangeStatus(int? id, bool? status)
+        {
+            var task = _context.Tasks.FirstOrDefault(p => p.Id == id);
+            if (task != null)
+            {
+                task.Status = status;
+                _context.Tasks.Update(task);
+                _context.SaveChanges();
+            }
+            return Json(new { html = Helper.RenderRazorViewToString(this, "_ViewAll", _context.Tasks.ToList()) });
+        }
+
+        private bool TaskModelExists(int id)
+        {
+            return _context.Tasks.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ToDoAppCore/Models/TaskModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoAppCore.Models
+{
+    public class TaskModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Date Issue")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime DateIssue { get; set; }
+
+        [Required]
+        [Display(Name = "Date End")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime DateEnd { get; set; }
+
+        public bool? Status { get; set; }
+    }
+}
